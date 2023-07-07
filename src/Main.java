8c409d34b785449e90b30bc6ff64e68a1dcb82cd
Ekaterina Evtyukhova <katyev60@gmail.com>
2023-07-07 12:32:41 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100; // начальная сумма на счете клиента
-        int depositmoney = 300; // сумма пополнения счета
+        int depositMoney = 300; // сумма пополнения счета
 
-        if (depositmoney < 1000) {
+        if (depositMoney < 1000) {
             System.out.println("Количество начисленных бонусов: 0");
             System.out.println("Итоговая сумма на счете:");
-            System.out.println(balance + depositmoney);
+            System.out.println(balance + depositMoney);
         } else {
             System.out.println("Количество начисленных бонусов:");
-            System.out.println(depositmoney / 100);
+            System.out.println(depositMoney / 100);
             System.out.println("Итоговая сумма на счете:");
-            System.out.println(depositmoney / 100 + balance + depositmoney);
+            System.out.println(depositMoney / 100 + balance + depositMoney);
         }
     }
 }
